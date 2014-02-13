667188c3679805d43290cf3392709efc0b55f72e
schollz <zack.scholl@gmail.com>
2014-02-12 19:04:30 -0500

    adding main files v.01

---
+++ parseVoiceText.sh
@@ -0,0 +1,1 @@
+wget -O - -o /dev/null --post-file out.flac --header="Content-Type: audio/x-flac; rate=16000" http://www.google.com/speech-api/v1/recognize?lang=en | sed -e 's/[{}]/''/g'| awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]; exit }' | awk -F: 'NR==3 { print $3; exit }' > txt.out
